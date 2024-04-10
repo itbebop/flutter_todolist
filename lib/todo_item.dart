d862f05ee50b7402212e6fde941156ed472f0179
icicle_4 <dwayne.han4@gmail.com>
2024-04-11 00:40:18 +0900

    chore: intl추가, DateFormat적용

--- lib/todo_item.dart
+++ lib/todo_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todolist/todo.dart';
+import 'package:intl/intl.dart';
 
 class TodoItem extends StatelessWidget {
   final Todo todo;
@@ -24,7 +25,8 @@ class TodoItem extends StatelessWidget {
         style: TextStyle(color: todo.isDone ? Colors.grey : Colors.black),
       ),
       subtitle: Text(
-        '${todo.dateTime}',
+        DateFormat.yMMMEd().format(DateTime.fromMillisecondsSinceEpoch(todo.dateTime)), // 한글로 바꾸려면 local 넣고 해야할듯
+        // db에는 DateTime넣을 수 없다?
         style: TextStyle(color: todo.isDone ? Colors.grey : Colors.black),
       ),
     );

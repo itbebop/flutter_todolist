6a6fb4f47c89783b0d2cd880efef53ca09205cdc
icicle_4 <dwayne.han4@gmail.com>
2024-04-10 22:10:57 +0900

    feat: todo 객체만들어서 위젯으로 변환

--- lib/list_screen.dart
+++ lib/list_screen.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todolist/create_screen.dart';
+import 'package:flutter_todolist/todo.dart';
 
-class ListScreen extends StatelessWidget {
+class ListScreen extends StatefulWidget {
   const ListScreen({super.key});
+
+  @override
+  State<ListScreen> createState() => _ListScreenState();
+}
+
+class _ListScreenState extends State<ListScreen> {
+  final todos = [
+    Todo(
+      title: 'title 1',
+      dateTime: 12321,
+    ),
+    Todo(
+      title: 'title 2',
+      dateTime: 123211231,
+    ),
+    Todo(
+      title: 'title 3',
+      dateTime: 12321124124,
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -17,16 +38,12 @@ class ListScreen extends StatelessWidget {
         child: const Icon(Icons.add),
       ),
       body: ListView(
-        children: const [
-          ListTile(
-            title: Text('title 1'),
-            subtitle: Text('subtitle 1'),
-          ),
-          ListTile(
-            title: Text('title 2'),
-            subtitle: Text('subtitle 2'),
-          ),
-        ],
+        children: todos
+            .map((todo) => ListTile(
+                  title: Text(todo.title),
+                  subtitle: Text('${todo.dateTime}'),
+                ))
+            .toList(),
       ),
     );
   }
---
+++ lib/todo.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Todo {
+  String title;
+  int dateTime;
+
+  Todo({
+    required this.title,
+    required this.dateTime,
+  });
+}

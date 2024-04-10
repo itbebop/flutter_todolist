8bbaed0068ec2bcda958754412413a1ddac665bb
icicle_4 <dwayne.han4@gmail.com>
2024-04-10 22:32:55 +0900

    feat: hive 세팅 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todolist/list_screen.dart';
+import 'package:flutter_todolist/todo.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main() {
+// 기능상 좋지 않으나 난이도 낮추기 위해서 static으로 선언
+late final Box<Todo> todos;
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(TodoAdapter());
+  todos = await Hive.openBox('todolist.db');
+  // 이 객체를 통해서 db에 접근할 수 있음
+
   runApp(const MyApp());
 }
 
--- lib/todo.dart
+++ lib/todo.dart
@@ -1,6 +1,17 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-class Todo {
+import 'package:hive/hive.dart';
+
+part 'todo.g.dart';
+
+@HiveType(typeId: 0)
+class Todo extends HiveObject {
+  @HiveField(0)
+  int? id;
+
+  @HiveField(1)
   String title;
+
+  @HiveField(2)
   int dateTime;
 
   Todo({
